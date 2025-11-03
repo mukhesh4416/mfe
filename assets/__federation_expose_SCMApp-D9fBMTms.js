592ad469ba0307e0f5a9a806f1a72d642a11965c
Venkata Mukesh <Venkata.mukhesh@lotuswireless.com>
2025-11-03 17:47:20 +0530

    Updates

--- assets/__federation_expose_SCMApp-D9fBMTms.js
+++
@@ -1,9 +0,0 @@
-import{importShared as l}from"./__federation_fn_import-BWWYUG6M.js";var i={exports:{}},e={};/**
- * @license React
- * react-jsx-runtime.production.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var o;function v(){if(o)return e;o=1;var R=Symbol.for("react.transitional.element"),p=Symbol.for("react.fragment");function s(d,r,t){var n=null;if(t!==void 0&&(n=""+t),r.key!==void 0&&(n=""+r.key),"key"in r){t={};for(var u in r)u!=="key"&&(t[u]=r[u])}else t=r;return r=t.ref,{$$typeof:R,type:d,key:n,ref:r!==void 0?r:null,props:t}}return e.Fragment=p,e.jsx=s,e.jsxs=s,e}var a;function E(){return a||(a=1,i.exports=v()),i.exports}var x=E();await l("react");function m(){return x.jsx(x.Fragment,{children:"SCM APP"})}export{m as default,x as j};
---
+++ assets/__federation_expose_SCMApp-DwQ_61ok.js
@@ -0,0 +1,9 @@
+import{importShared as y}from"./__federation_fn_import-DpuBP_xz.js";import{r as O}from"./index-ChsGqxH_.js";var i={exports:{}},t={};/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var a;function h(){if(a)return t;a=1;var c=O(),R=Symbol.for("react.element"),x=Symbol.for("react.fragment"),d=Object.prototype.hasOwnProperty,l=c.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,v={key:!0,ref:!0,__self:!0,__source:!0};function s(n,r,p){var e,o={},u=null,f=null;p!==void 0&&(u=""+p),r.key!==void 0&&(u=""+r.key),r.ref!==void 0&&(f=r.ref);for(e in r)d.call(r,e)&&!v.hasOwnProperty(e)&&(o[e]=r[e]);if(n&&n.defaultProps)for(e in r=n.defaultProps,r)o[e]===void 0&&(o[e]=r[e]);return{$$typeof:R,type:n,key:u,ref:f,props:o,_owner:l.current}}return t.Fragment=x,t.jsx=s,t.jsxs=s,t}var m;function j(){return m||(m=1,i.exports=h()),i.exports}var _=j();await y("react");function q(){return _.jsx(_.Fragment,{children:"SCM APP"})}export{q as default,_ as j};
