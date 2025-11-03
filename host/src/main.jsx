08777b835c47337d5c7f03758c5b9002548b0619
Venkata Mukesh <Venkata.mukhesh@lotuswireless.com>
2025-11-03 14:22:12 +0530

    changes

--- host/src/main.jsx
+++ host/src/main.jsx
@@ -4,10 +4,6 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 
-import 'primereact/resources/themes/bootstrap4-light-blue/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
-
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
